123dec964ccb7ac6d3fa8e53467bb124b72fbfcc
p2401kumar@gmail.com <p2401kumar.com>
2019-03-14 18:17:44 -0700

    Composite Pattern

---
+++ DP/src/com/samsung/CompositePattern/Main9.java
@@ -0,0 +1,100 @@
+package com.samsung.CompositePattern;
+
+import java.util.ArrayList;
+
+abstract class Component {
+    private String name;
+
+    public Component(/*Component this*/String name) {
+        this.name = name;
+    }
+
+    public String getName(/*Component this*/) {
+        return name;
+    }
+
+    public void show(/*Component this*/) {
+        System.out.println(name + " show");
+    }
+
+    public void hide(/*Component this*/) {
+        System.out.println(name + " hide");
+    }
+}
+
+//every composite is a Component
+abstract class Composite extends Component {
+    ArrayList<Component> components = new ArrayList<>();
+
+    public Composite(/*this*/String name) {
+        super(name);
+    }
+
+    public void add(Component component) {
+        components.add(component);
+    }
+
+    public void show(/*Component this*/) {
+        super.show(/*value of this*/);
+        for (Component component: components) {
+            component.show();
+        }
+    }
+
+    public void hide(/*Component this*/) {
+        super.hide(/*value of this*/);
+        for (Component component: components) {
+            component.hide();
+        }
+    }
+}
+
+class Window extends Composite {
+    public Window(String name) {
+        super(name);
+    }
+}
+
+class Panel extends Composite {
+    public Panel(String name) {
+        super(name);
+    }
+}
+
+class Button extends Component {
+    public Button(String name) {
+        super(name);
+    }
+}
+
+public class Main9 {
+    public static void main(String[] args) {
+        Window w1 = new Window("w1");
+        Button b1 = new Button("b1");
+        Button b2 = new Button("b2");
+
+        w1.add(b1);
+        w1.add(b2);
+
+        w1.show();
+        System.out.println("----------------");
+        w1.hide();
+        System.out.println("----------------");
+
+        Panel p1 = new Panel("p1");
+        Button b3 = new Button("b3");
+        Button b4 = new Button("b4");
+
+        p1.add(b3);
+        p1.add(b4);
+
+        w1.add(p1);
+        w1.show();
+        System.out.println("----------------");
+        p1.hide();
+        System.out.println("----------------");
+        w1.show();
+        System.out.println("----------------");
+
+    }
+}
